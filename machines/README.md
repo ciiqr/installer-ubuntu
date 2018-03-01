d2f498bb5c477351e7d0ec392c768ec103590cc6
William Villeneuve <willvill1995@yahoo.ca>
2018-02-28 23:54:44 -0500

    config migration: removed files we definitely won't need

--- machines/README.md
+++
@@ -1,5 +0,0 @@
-# machines
-## To setup a new machine
-- create categories/preseed-MACHINE.cfg
-- create categories/preseed-MACHINE.inc
-- modify scripts/inc/setup-categories.sh
--- machines/generate-test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# usage:
-# ./generate-test.sh --private-config ~/.private-config --default-machine server-data --build d
-
-# TODO: At some point I should see if I can have it auto create the vm... Can I use vagrant in a non-headless fashion?
-VBoxManage startvm ubuntis 2> /dev/null
-./generate.sh "$@" && VBoxManage controlvm ubuntis reset
