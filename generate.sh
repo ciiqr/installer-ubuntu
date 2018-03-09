a73416d8b39f8215cc1fdf39ef78604faf0953c1
William Villeneuve <willvill1995@yahoo.ca>
2018-03-08 23:37:43 -0500

    migrated development from machines to config

--- generate.sh
+++ generate.sh
@@ -200,6 +200,14 @@ done
 # Set default boot option
 if [[ ! -z "$IMAGE_DEFAULT_MACHINE" ]]; then
     # TODO: Replace with replace_or_append if applicable...
+    # replace_or_append()
+    # {
+    #     match_line="$2"
+    #     replace_line="$3"
+    #     # TODO: I would like to be able to replace with some matched part... (untested)
+    #     # TODO: May also be good to only replace the first match... if reasonable...
+    #     sed -i '/^'"$match_line"'/{h;s/.*/'"$replace_line"'/};${x;/^$/{s//'"$replace_line"'/;H};x}' "$1"
+    # }
     sudo sed 's/default .*/default '"$IMAGE_DEFAULT_MACHINE"'/' -i "$IMAGE_ISOLINUX_MENU_CONFIG"
 fi
 
